7f7ae2b03f62549039b29a43285559457358dae4
Danil <matrosovdan@mail.ru>
2023-03-05 21:31:45 +0300

    Start page is working!

--- src/main/java/ru/alishev/springcourse/controllers/indexController.java
+++ src/main/java/ru/alishev/springcourse/controllers/indexController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping()
 public class indexController {
-    @GetMapping("/")
+    @GetMapping()
     public String index() {
-        return "index";
+        return "people/index";
     }
 }
